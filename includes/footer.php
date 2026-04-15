b9853fefd2fee6ec04c8dd9ba01d636e65f4b59f
ESHO <ajchive.23@gmail.com>
2026-04-15 19:22:54 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+</main>
+<footer class="site-footer container">
+  <div class="footer-grid">
+    <p>&copy; <?php echo date("Y"); ?> Crazy Florist. All rights reserved.</p>
+    <div class="social-icons">
+      <a href="#"><i class="fab fa-facebook"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
